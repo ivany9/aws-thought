df5b60feb808a229f89f91e0d2accc5bf9814f2a
jorge rodriguez <ivany9@gmail.com>
2022-01-07 09:27:38 +1100

    chan

---
+++ client/build/precache-manifest.a5ec967627106ecdcd7ec31cecb7ef7a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb32ddc4ba95bc1a96fe5bacc94affdf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8068585b8b119b8dad60",
+    "url": "/static/css/main.c5ae7a51.chunk.css"
+  },
+  {
+    "revision": "97e6a5419a7cce36adcf",
+    "url": "/static/js/2.c89f58b6.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.c89f58b6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8068585b8b119b8dad60",
+    "url": "/static/js/main.eccc38f5.chunk.js"
+  },
+  {
+    "revision": "c71e5690aa6c32254dd7",
+    "url": "/static/js/runtime-main.758ba164.js"
+  }
+]);
